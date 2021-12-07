ac982f5873b690012ffbfc8f07662c18fcaffa23
Seongjin Lee <babo318@gmail.com>
2021-12-07 20:47:05 +0900

    circle 스키마 입니다

---
+++ schema/circlesSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const circlesSchema = new mongoose.Schema({
+    projects_id: {
+        type: Number,
+        unique: true,
+        required: true,
+    },
+    circles_id: {
+        type: String,
+        unique: true,
+    },
+    feedback: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Number,
+    }
+});
+
+module.exports = mongoose.model("circles", circlesSchema);
+
