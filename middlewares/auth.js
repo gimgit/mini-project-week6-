f210c40210314a5adb71f6313f23880313769e00
Seongjin Lee <babo318@gmail.com>
2021-12-07 20:50:49 +0900

    수정본입니다

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,16 +2,16 @@ const JWT = require("jsonwebtoken");
 // const users = require("./shcemas/userSchema");
 
 module.exports = async (req, res, next) => {
-    console.log("여기를 지나서");
-    next();
-    // try {
-    //     const token = await req.headers["authoriztion"]
-    //     const [type, encodedToken] = token.split(" ");
-    //     const userID = JWT.verify(encodedToken, "secret-key");
-    //     res.locals = userID;
-    //     next();
-    // } catch {
-    //     res.status(403).send();
-    //     return;
-    // }
+    // console.log("여기를 지나서");
+    // next();
+    try {
+        const token = await req.headers["authoriztion"]
+        const [type, encodedToken] = token.split(" ");
+        const userID = JWT.verify(encodedToken, "secret-key");
+        res.locals = userID;
+        next();
+    } catch {
+        res.status(403).send();
+        return;
+    }
 }
