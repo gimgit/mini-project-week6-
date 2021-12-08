eff06c7281d4387a0e7e173beaaa54d5a2b57fdb
Seongjin Lee <babo318@gmail.com>
2021-12-08 22:57:49 +0900

    auth 오타 수정

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,13 +5,14 @@ module.exports = async (req, res, next) => {
     // console.log("여기를 지나서");
     // next();
     try {
-        const token = await req.headers["authoriztion"];
+        const token = await req.headers["authorization"];
+        console.log(token);
         const [type, encodedToken] = token.split(" ");
-        const userID = JWT.verify(encodedToken, "99td");
+        const userID = JWT.verify(encodedToken, "secret-key");
         res.locals = userID;
         next();
     } catch {
-        res.status(403).send();
+        res.status(403).send("유효하지 않은 사용자입니다");
         return;
     }
-};
+}
